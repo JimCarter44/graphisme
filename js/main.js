546dd65d65e1996a59eb4c9640bd4d35d2a21228
JimCarter44 <gontard.j@hotmail.fr>
2022-04-21 17:20:59 -0400

    added test img

--- js/main.js
+++ js/main.js
@@ -3,10 +3,21 @@ $(document).ready(function () {
   let fullScreenImg = $(".fullScreenImg");
   let doneCountryFull = $("#donCountryFull");
   let weazelFull = $("#weazelFull");
+  let afficheJCP = $("#afficheJCPFull");
+  let fourFull = $("#fourFull");
   $(".fa-xmark").click(function () {
     fullScreenImg.fadeOut(500);
     weazelFull.fadeOut(500);
     doneCountryFull.fadeOut(500);
+    afficheJCP.fadeOut(500);
+    fourFull.fadeOut(500);
+  });
+  $("#fourLink").click(function () {
+    console.log("click");
+    fullScreenImg.fadeIn(500);
+    fourFull.fadeIn(500);
+    fullScreenImg.css("display", "flex");
+    fourFull.css("display", "flex");
   });
   $("#weazelLink").click(function () {
     console.log("click");
@@ -14,6 +25,13 @@ $(document).ready(function () {
     weazelFull.fadeIn(500);
     fullScreenImg.css("display", "flex");
     weazelFull.css("display", "flex");
+  });
+  $("#afficheJCPLink").click(function () {
+    console.log("click");
+    fullScreenImg.fadeIn(500);
+    afficheJCP.fadeIn(500);
+    fullScreenImg.css("display", "flex");
+    afficheJCP.css("display", "flex");
   });
   $("#donCountryLink").click(function () {
     console.log("click");
